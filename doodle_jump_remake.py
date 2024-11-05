5c56eac591aec4289a56e0857d938f8de059e3a4
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2024-11-05 17:35:40 +0300

    0.01

---
+++ doodle_jump_remake.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+import math
+import sys
+pygame.init()
+
+sc = pygame.display.set_mode((800, 500), pygame.RESIZABLE)
+
+
+
+clock = pygame.time.Clock()
+FPS = 60
+while_activity = True
+while while_activity:
