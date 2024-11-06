b6281b8a194b47c0db89b4ddfe0aadd518e5ba1b
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-11-06 13:55:59 +0600

    jump platform x, y removed

--- jump_platform.py
+++ jump_platform.py
@@ -3,9 +3,8 @@ from player import Player
 
 class JumpPlatform(pygame.sprite.Sprite):
     def __init__(self, x, y, player:Player):
-        self.x = x
-        self.y = y
         self.player = player
+        
         self.image = pygame.image.load('images/jump_platform.png').convert()
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect(center=(x, y))
@@ -15,4 +14,4 @@ class JumpPlatform(pygame.sprite.Sprite):
             self.player.jump()
 
     def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, self.rect.center)
