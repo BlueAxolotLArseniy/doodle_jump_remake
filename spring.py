f4cd4c3a27a4f43a4ab6c2fc5ffb194288cd06bc
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2024-11-07 13:20:58 +0600

    spring spawn fix + jump force edited

--- spring.py
+++ spring.py
@@ -1,4 +1,5 @@
 
+import random
 import pygame
 from jump_platform import JumpPlatform
 from player import Player
@@ -6,10 +7,11 @@ from player import Player
 
 class SpringPlatform(JumpPlatform):
     def __init__(self, x, y, player: Player):
-        self.spring_image = pygame.image.load("images/spring.png")
-        self.spring_image_rect = self.spring_image.get_rect(center=(x, y-20))
-        self.jump_force = 2.0
         super().__init__(x, y, player)
+        self.spring_image = pygame.image.load("images/spring.png")
+        rand_x = random.randint(x+3, x+67)
+        self.spring_image_rect = self.spring_image.get_rect(center=(rand_x, y-20))
+        self.jump_force = 1.3
 
     def update(self):
         if self.player.rect.colliderect(self.rect) and self.player.real_speed <= -20:
